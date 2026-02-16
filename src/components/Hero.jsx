71d257573ba0d4b5b891eb74503700615a213daf
sadhin28 <taosifsadhin113@gmail.com>
2026-02-16 16:27:42 +0600

    add home page title and subtitle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,42 +20,9 @@ const Hero = () => {
   };
   return (
     <div id="home" className="">
-       <div className="flex  flex-col md:flex-row justify-around items-center  px-4">
-          {/* Left div */}  
-      <div className="mt-20 md:mt-20 md:w-1/2 text-center md:text-left">
-        <div className="space-y-4">
-          <h1 className="md:text-xl lg:text-4xl font-bold leading-tight animate-fade-in-up">
-            &nbsp;
-            <TypeAnimation
-              sequence={[
-                "We Build Stunning Websites That Convert",
-                2000
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-              className="text-[#26b0f0]"
-            />
-          </h1>
-
-          <p className="text-gray-400 md:max-w-md text-sm md:text-xl text-muted-foreground max-3xl ">
-            Professional Web Design & Development Agency</p>
-        </div>
-      </div>
-
-      {/* Right div */}
-      <div className=" mt-10 md:mt-40 w-60 h-60 sm:w-72 sm:h-72 md:w-80 md:h-80 relative">
-        {/* Animated gradient border */}
-        <div className="absolute inset-0 rounded-full p-[3px] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 to-red-500 to-green-500">
-          <div className="w-full h-full rounded-full bg-white flex items-center justify-center">
-            <img
-              className="rounded-full object-cover w-full h-full animate-float"
-              src={profileImg && profileImg}
-              alt="profile"
-            />
-          </div>
-        </div>
-      </div>
+       <div className="">
+        <h1 className="text-1xl text-center text-blue-500 pt-40 pb-5">Is.Nex Web Solution</h1>
+        <h2 className="text-3xl md:text-5xl lg:text-7xl font-bold text-center justify-center">We Build Stunning <br />Websites That Convert</h2>
        </div>
        <div className="flex mt-10 sm:flex-row gap-20 justify-center items-center animate-fade-in-up animate-delay-400 ">
           <ReactiveButton outline
